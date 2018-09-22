bfd67a9a0a061c66ca2cf3333b503196b94a8b17
Majid Jaidi <majid.jaidi@gmail.com>
2018-09-22 14:17:05 +0100

    removed middleman-bloging stuff

--- config.rb
+++ config.rb
@@ -23,47 +23,10 @@ activate :deploy do |deploy|
   deploy.deploy_method = :git
 end
 
-activate :blog do |blog|
-  # This will add a prefix to all links, template references and source paths
-  blog.name = "projets"
-  blog.prefix = "projets"
-
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
-  # Matcher for blog source files
-  blog.sources = "{year}-{month}-{day}_{title}.html"
-  # blog.taglink = "tags/{tag}.html"
-  blog.layout = "blog_layout"
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
-  # blog.year_link = "{year}.html"
-  # blog.month_link = "{year}/{month}.html"
-  # blog.day_link = "{year}/{month}/{day}.html"
-  blog.default_extension = ".md"
-
-  blog.tag_template = "tag.html"
-  blog.calendar_template = "calendar.html"
-
-  # Enable pagination
-  # blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/{num}"
-end
-
-activate :blog do |blog|
-  # This will add a prefix to all links, template references and source paths
-  blog.name = "reviews"
-  blog.prefix = "reviews"
-
-  blog.sources = "{year}-{month}-{day}_{title}.html"
-  blog.layout = "blog_layout"
-
-  blog.default_extension = ".md"
-
-  blog.tag_template = "tag.html"
-  blog.calendar_template = "calendar.html"
-end
-
-
 activate :dato,
   token: '84c24daaaaace03672cc48af61d6e2',
   base_url: 'https://wonderful-sammet-fd7ce6.netlify.com'
+
+dato.projets.each_with_index do |_info, i|
+  proxy "/projets/#{i}.html", "/projets/show.html", locals: { index: i }, ignore: true
+end
--- source/projets/2018-09-20_projet-non-publi-4.html.md
+++
@@ -1,9 +0,0 @@
----
-title: Projet non publié 4
-description: non publié
-duree: 5 mois
-thumbnail: /imgs/uploads/paul-hanaoka-369639-unsplash.jpg
-publish: Non
-category: contenaires
----
-
